56581cb45c41476bb5e88b05c588c858fadae2d0
Anton Kozyriev <sonara4mus@gmail.com>
2020-11-08 18:43:55 +0200

    wordcloud generation from repositories topics

--- cloud/wordcloud_generator.py
+++ cloud/wordcloud_generator.py
@@ -1,6 +1,13 @@
+# AUTOMATED SCRIPT FOR GENERATING WORDCLOUD
+# FOR README.MD FROM TOPICS OF PUBLIC REPOSITORIES
+
 import sys
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud
 from json import loads
 from requests import get
+
 
 AUTH_TOKEN = sys.argv[1]
 PROFILE_NAME = 'antonace'
@@ -11,15 +18,27 @@ GITHUB_ACCEPTABLE_HEADERS = \
         'Authorization': f'Bearer {AUTH_TOKEN}'
     }
 
+WORDCLOUD_IMAGE_FILE_PATH = 'wordcloud.png'
 
-def parse_topics(repository_name: str) -> list:
+
+def fetch_topics(repository_name: str) -> list:
     topics_response = get(f'https://api.github.com/repos/{PROFILE_NAME}/{repository_name}/topics',
                           headers=GITHUB_ACCEPTABLE_HEADERS)
     topics = loads(str(topics_response.text))
-    return topics["names"]
+    return [topic.replace('-', '_') for topic in topics["names"]]
 
+
+print('FETCHING REPOSITORIES\' TOPICS')
 
 response = get(f'https://api.github.com/users/{PROFILE_NAME}/repos', headers=GITHUB_ACCEPTABLE_HEADERS)
 repositories = loads(str(response.text))
-repositories_topics = [' '.join(parse_topics(repo["name"])) for repo in repositories]
+repositories_topics = [' '.join(fetch_topics(repo["name"])) for repo in repositories]
 joined_topics = ' '.join(repositories_topics)
+
+
+print('GENERATING WORDCLOUD FROM TOPICS')
+
+cloud = WordCloud(width=800, height=600, background_color='white', colormap="rainbow").generate(joined_topics)
+cloud.to_file(WORDCLOUD_IMAGE_FILE_PATH)
+
+print('WORDCLOUD SAVED SUCCESSFULLY')
