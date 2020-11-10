79402e283fb0e2e7d07f8a7f2d3172debe5525e5
Anton Kozyriev <sonara4mus@gmail.com>
2020-11-10 17:49:38 +0200

    revert recoloring

--- scripts/wordcloud_generator.py
+++ scripts/wordcloud_generator.py
@@ -43,15 +43,13 @@ print('GENERATING WORDCLOUD FROM TOPICS')
 
 wordcloud_location = WORDCLOUD_IMAGE_FILE_PATH + '/' + WORDCLOUD_IMAGE_FILE_NAME
 wordcloud_image = np.array(Image.open(WORDCLOUD_MASK_FILE_PATH))
-wordcloud_image = np.where(wordcloud_image == 0, 255, wordcloud_image)
 
 stopwords = set(STOPWORDS)
 
 if not os.path.exists(WORDCLOUD_IMAGE_FILE_PATH):
     os.makedirs(WORDCLOUD_IMAGE_FILE_PATH)
 
-cloud = WordCloud(mask=wordcloud_image, background_color='white',
-                  stopwords=stopwords, max_words=1000).generate(joined_topics)
+cloud = WordCloud(mask=wordcloud_image, background_color='white').generate(joined_topics)
 
 cloud_color_scheme = ImageColorGenerator(wordcloud_image)
 cloud.recolor(color_func=cloud_color_scheme)
