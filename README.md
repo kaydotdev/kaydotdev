0a27a4ebe6647999d4592f719e56fd4c0048e48b
Anton Kozyriev <sonara4mus@gmail.com>
2021-06-30 18:47:14 +0300

    change wordcloud mask to nickname picture

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-<h1><img src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/gif/greeter.gif" width="800px"></h1>
-
-<img align="right" alt="octocloud" width="300px" src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/png/wordcloud.png">
-
-<a>My name is Anton! I'm a student, interested in the field of <strong>Cloud Technologies</strong> and <strong>Artificial intelligence</strong>!</a>
+<h1><img src="static/png/wordcloud.png"></h1>
 
 <ul>
-  <li>:computer: My primary stack is <strong>.NET</strong> :heavy_plus_sign: <strong>Typescript</strong> :heavy_plus_sign: <strong>Python</strong>;</li>
   <li>:ledger: I’m getting my master’s in Data Science and Mathematical modelling;</li>
   <li>:microscope: I'm working on a <a href="https://github.com/antonAce/adaptive-gradient-descent">research</a> for optimization algorithms;</li>
   <li>:handshake: I'm looking for a collaboration / contribution for the projects;</li>
   <li>:seedling: I learn everything related to the <strong>Big Data</strong> and <strong>AI</strong>;
-  <li>:relaxed: In my free time I like to play video games and read literature. My most liked genres are Fantasy, Sci-Fi and Post-apocalyptic;</li>
-  <li>:floppy_disk: I use <a href="https://manjaro.org/">Manjaro Linux</a> on a daily basis;</li>
-  <li>:fire: <strong>'Continuous learning / continuous self-improvement'</strong> is my strategy of life;</li>
 </ul>
 
 <br>
--- scripts/wordcloud_generator.py
+++ scripts/wordcloud_generator.py
@@ -3,7 +3,6 @@
 
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
@@ -30,7 +29,7 @@ def fetch_topics(repository_name: str) -> list:
     topics_response = get(f'https://api.github.com/repos/{PROFILE_NAME}/{repository_name}/topics',
                           headers=GITHUB_ACCEPTABLE_HEADERS)
     topics = loads(str(topics_response.text))
-    return [topic.replace('-', '.') for topic in topics['names']]
+    return [topic.replace('-', ' ').strip() for topic in topics['names']]
 
 
 print('FETCHING REPOSITORIES\' TOPICS')
@@ -38,7 +37,8 @@ print('FETCHING REPOSITORIES\' TOPICS')
 response = get(f'https://api.github.com/users/{PROFILE_NAME}/repos', headers=GITHUB_ACCEPTABLE_HEADERS)
 repositories = loads(str(response.text))
 repositories_topics = [' '.join(fetch_topics(repo['name'])) for repo in repositories]
-joined_topics = ' '.join(repositories_topics)
+joined_topics = ' '.join([repo for repo in repositories_topics if len(repo) != 0])
+wordcloud_text = joined_topics + ' ' + ' '.join([repo['description'] for repo in repositories if repo['description'] is not None])
 
 print('GENERATING WORDCLOUD FROM TOPICS')
 
@@ -51,7 +51,7 @@ if not os.path.exists(WORDCLOUD_IMAGE_FILE_PATH):
 cloud = WordCloud(background_color="white", repeat=True,
                   mask=wordcloud_image, font_path=WORDCLOUD_FONT_PATH)
 
-cloud.generate(joined_topics)
+cloud.generate(wordcloud_text)
 cloud_color_scheme = ImageColorGenerator(wordcloud_image)
 cloud.recolor(color_func=cloud_color_scheme)
 
