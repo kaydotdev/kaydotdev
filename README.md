6b5cedec0079e77a07e910e613ddcffb6d54fcea
Anton Kozyriev <sonara4mus@gmail.com>
2021-07-03 19:17:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1><img src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/gif/greeter.gif" width="800px"></h1>
+
+<img align="right" alt="octocloud" width="300px" src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/png/wordcloud.png">
+
+<a>My name is Anton! I'm a student, interested in the field of <strong>Cloud Technologies</strong> and <strong>Artificial intelligence</strong>!</a>
+
+<ul>
+  <li>:computer: My primary stack is <strong>.NET</strong> :heavy_plus_sign: <strong>Typescript</strong> :heavy_plus_sign: <strong>Python</strong>;</li>
+  <li>:ledger: I’m getting my master’s in Data Science and Mathematical modelling;</li>
+  <li>:microscope: I'm working on a <a href="https://github.com/antonAce/adaptive-gradient-descent">research</a> for optimization algorithms;</li>
+  <li>:handshake: I'm looking for a collaboration / contribution for the projects;</li>
+  <li>:seedling: I learn everything related to the <strong>Big Data</strong> and <strong>AI</strong>;
+  <li>:relaxed: In my free time I like to play video games and read literature. My most liked genres are Fantasy, Sci-Fi and Post-apocalyptic;</li>
+  <li>:floppy_disk: I use <a href="https://manjaro.org/">Manjaro Linux</a> on a daily basis;</li>
+  <li>:fire: <strong>'Continuous learning / continuous self-improvement'</strong> is my strategy of life;</li>
+</ul>
+
+<br>
+
+<a href="https://github.com/antonAce?tab=repositories">
+  <img align="left" alt="Github" width="25px" src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/svg/github.svg" />
+</a>
+<a href="https://www.linkedin.com/in/anton-kozyriev-66b272166">
+  <img align="left" alt="LinkedIn" width="25px" src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/svg/linkedin.svg" />
+</a>
+<a href="https://t.me/anton_on_github_bot">
+  <img align="left" alt="Telegram" width="25px" src="https://raw.githubusercontent.com/antonAce/antonAce/master/static/svg/telegram.svg" />
+</a>
---
+++ scripts/wordcloud_generator.py
@@ -0,0 +1,60 @@
+# AUTOMATED SCRIPT FOR GENERATING WORDCLOUD
+# FOR README.MD FROM TOPICS OF PUBLIC REPOSITORIES
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
+from json import loads
+from requests import get
+
+
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+PROFILE_NAME = 'antonace'
+GITHUB_ACCEPTABLE_HEADERS = \
+    {
+        'Accept': 'application/vnd.github.mercy-preview+json',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Authorization': f'Bearer {AUTH_TOKEN}'
+    }
+
+WORDCLOUD_FONT_PATH = 'static/font/firacode.ttf'
+WORDCLOUD_IMAGE_FILE_PATH = 'static/png'
+WORDCLOUD_MASK_FILE_PATH = WORDCLOUD_IMAGE_FILE_PATH + '/' + 'wordcloud_mask.png'
+WORDCLOUD_IMAGE_FILE_NAME = 'wordcloud.png'
+
+
+def fetch_topics(repository_name: str) -> list:
+    topics_response = get(f'https://api.github.com/repos/{PROFILE_NAME}/{repository_name}/topics',
+                          headers=GITHUB_ACCEPTABLE_HEADERS)
+    topics = loads(str(topics_response.text))
+    return [topic.replace('-', '.') for topic in topics['names']]
+
+
+print('FETCHING REPOSITORIES\' TOPICS')
+
+response = get(f'https://api.github.com/users/{PROFILE_NAME}/repos', headers=GITHUB_ACCEPTABLE_HEADERS)
+repositories = loads(str(response.text))
+repositories_topics = [' '.join(fetch_topics(repo['name'])) for repo in repositories]
+joined_topics = ' '.join(repositories_topics)
+
+print('GENERATING WORDCLOUD FROM TOPICS')
+
+wordcloud_location = WORDCLOUD_IMAGE_FILE_PATH + '/' + WORDCLOUD_IMAGE_FILE_NAME
+wordcloud_image = np.array(Image.open(WORDCLOUD_MASK_FILE_PATH))
+
+if not os.path.exists(WORDCLOUD_IMAGE_FILE_PATH):
+    os.makedirs(WORDCLOUD_IMAGE_FILE_PATH)
+
+cloud = WordCloud(background_color="white", repeat=True,
+                  mask=wordcloud_image, font_path=WORDCLOUD_FONT_PATH)
+
+cloud.generate(joined_topics)
+cloud_color_scheme = ImageColorGenerator(wordcloud_image)
+cloud.recolor(color_func=cloud_color_scheme)
+
+cloud.to_file(wordcloud_location)
+
+print('WORDCLOUD SAVED SUCCESSFULLY')
